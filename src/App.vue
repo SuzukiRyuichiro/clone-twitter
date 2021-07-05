499c6a11f491ebbcf988e79bc0f94d67d7a204d2
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-07-05 19:28:40 +0900

    font-awsome in works

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <!-- side nav -->
     <div class="w-1/5 border-r border-lighter px-8 py-2">
       <button class="h-12 w-12 hover:bg-lightblue text-3xl text-blue">
-        test
+        <font-awesome-icon icon="user-secret" />
       </button>
     </div>
   </div>
--- src/main.js
+++ src/main.js
@@ -3,4 +3,12 @@ import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
 
-createApp(App).use(router).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUserSecret)
+
+const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.use(router).mount('#app')
