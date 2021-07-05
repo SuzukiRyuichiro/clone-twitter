cb50098c3a3b1d2bcbee22952e7297a958ce0209
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-07-05 19:06:43 +0900

    cleaned stuff

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
   </div>
-  <router-view/>
 </template>
+
+<script>
+  export default {
+    name: 'app',
+    components: {
+    },
+    data() {
+      return {
+
+      }
+    },
+  }
+</script>
 
 <style>
 #app {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
+    container: {
+      center: true
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'blue': '#1DA1F2',
+        'darkblue': '#2795D9',
+        'lightblue': '#EFF9FF',
+        'dark': '#657786',
+        'light': '#AAB8C2',
+        'lighter': '#E1E8ED',
+        'lightest': '#F5F8FA',
+      }
+    },
   },
   plugins: [],
 }
