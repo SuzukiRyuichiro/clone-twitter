b8f8e87ef603a6f429e3f0d311a6f79d4cab96f7
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-07-05 19:08:51 +0900

    side nav in sight

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <div id="app">
+  <div id="app" class="flex container h-screen w-full">
+    <!-- side nav -->
+    <div class="w-1/5 border-r border-lighter px-8 py-2">
+      <button class="h-12 w-12 hover:bg-lightblue text-3xl text-blue">
+        test
+      </button>
+    </div>
   </div>
 </template>
 
